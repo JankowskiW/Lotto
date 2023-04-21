60a8ebc11b690a3b4e48e24c17b104d59c7645e0
jankowskiw <waldemar.jankowski95@gmail.com>
2023-04-21 23:21:12 +0200

    Create NotificationPort bean in TicketConfig file.

--- src/main/java/pl/wj/lotto/infrastructure/application/rest/ticket/TicketConfig.java
+++ src/main/java/pl/wj/lotto/infrastructure/application/rest/ticket/TicketConfig.java
@@ -2,22 +2,31 @@ package pl.wj.lotto.infrastructure.application.rest.ticket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.wj.lotto.domain.common.notification.EmailNotificationPort;
+import pl.wj.lotto.domain.common.notification.NotificationPort;
 import pl.wj.lotto.domain.ticket.adapter.TicketServiceAdapter;
 import pl.wj.lotto.domain.ticket.port.in.TicketServicePort;
 import pl.wj.lotto.domain.ticket.port.out.TicketRepositoryPort;
 import pl.wj.lotto.domain.ticket.service.TicketService;
+import pl.wj.lotto.infrastructure.notification.inmemory.email.EmailNotificationAdapter;
 import pl.wj.lotto.infrastructure.persistence.inmemory.ticket.TicketInMemoryAdapter;
 
 @Configuration
 public class TicketConfig {
     @Bean
-    public TicketServicePort ticketServicePort(TicketRepositoryPort ticketRepositoryPort) {
-        TicketService ticketService = new TicketService(null, ticketRepositoryPort);
+    public TicketServicePort ticketServicePort(TicketRepositoryPort ticketRepositoryPort,
+                                               NotificationPort notificationPort) {
+        TicketService ticketService = new TicketService(notificationPort, ticketRepositoryPort);
         return new TicketServiceAdapter(ticketService);
     }
 
     @Bean
     public TicketRepositoryPort ticketRepositoryPort() {
         return new TicketInMemoryAdapter();
+    }
+
+    @Bean
+    public NotificationPort notificationPort() {
+        return new EmailNotificationAdapter();
     }
 }
