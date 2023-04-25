5935f037e3d3dec33a01efc47edf47434b95779a
jankowskiw <waldemar.jankowski95@gmail.com>
2023-04-25 21:08:00 +0200

    Remove EntityScan annotation from LottoApplication.java

--- src/main/java/pl/wj/lotto/infrastructure/application/LottoApplication.java
+++ src/main/java/pl/wj/lotto/infrastructure/application/LottoApplication.java
@@ -2,12 +2,10 @@ package pl.wj.lotto.infrastructure.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {"pl.wj.lotto.infrastructure.persistence.database"})
-@EntityScan(basePackages = {"pl.wj.lotto.infrastructure.persistence.database"})
 public class LottoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LottoApplication.class, args);
