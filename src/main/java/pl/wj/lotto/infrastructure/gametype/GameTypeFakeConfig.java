1df61cc5c06631c6f4259f174f4cd09c202ea9b5
jankowskiw <waldemar.jankowski95@gmail.com>
2023-05-10 22:45:48 +0200

    Create fake config for gametype

--- src/main/java/pl/wj/lotto/infrastructure/gametype/GameTypeFakeConfig.java
+++ src/main/java/pl/wj/lotto/infrastructure/gametype/GameTypeFakeConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Profile;
 import pl.wj.lotto.domain.common.gametype.GameType;
 import pl.wj.lotto.domain.common.gametype.GameTypeSettingsContainer;
 import pl.wj.lotto.infrastructure.gametype.properties.interval.GameTypeIntervalProperties;
-import pl.wj.lotto.infrastructure.gametype.properties.settings.*;
+import pl.wj.lotto.infrastructure.gametype.properties.settings.EjpSettingsProperties;
+import pl.wj.lotto.infrastructure.gametype.properties.settings.GameTypeSettingsProperties;
+import pl.wj.lotto.infrastructure.gametype.properties.settings.KenoSettingsProperties;
+import pl.wj.lotto.infrastructure.gametype.properties.settings.LottoSettingsProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,17 +19,13 @@ import java.util.Map;
 public class GameTypeFakeConfig {
     @Bean
     public GameTypeSettingsContainer gameTypeSettingsContainer() {
-        LottoSettingsProperties lottoSettingsProperties = new LottoSettingsProperties();
-        Q600SettingsProperties q600SettingsProperties = new Q600SettingsProperties();
-        EjpSettingsProperties ejpSettingsProperties = new EjpSettingsProperties();
-        KenoSettingsProperties kenoSettingsProperties = new KenoSettingsProperties();
-        GameTypeIntervalProperties gameTypeIntervalProperties = GameTypeIntervalProperties.builder().build();
+        GameTypeIntervalProperties gameTypeIntervalProperties = gameTypeIntervalProperties();
 
         Map<GameType, GameTypeSettingsProperties> settings = new HashMap<>();
-        settings.put(GameType.LOTTO, lottoSettingsProperties);
-        settings.put(GameType.Q600, q600SettingsProperties);
-        settings.put(GameType.EJP, ejpSettingsProperties);
-        settings.put(GameType.KENO, kenoSettingsProperties);
+        settings.put(GameType.LOTTO, lottoSettingsProperties());
+        settings.put(GameType.Q600, q600SettingsProperties());
+        settings.put(GameType.EJP, ejpSettingsProperties());
+        settings.put(GameType.KENO, kenoSettingsProperties());
 
         Map<GameType, String> intervals = new HashMap<>();
         intervals.put(GameType.LOTTO, gameTypeIntervalProperties.lotto());
@@ -38,5 +37,51 @@ public class GameTypeFakeConfig {
                 .intervals(intervals)
                 .settings(settings)
                 .build();
+    }
+
+    private GameTypeIntervalProperties gameTypeIntervalProperties() {
+        return GameTypeIntervalProperties.builder()
+                .lotto("0 0 22 * * 2,4,6")
+                .q600("0 2-58/4 6-22 * * *")
+                .ejp("0 0 21 * * 2,5")
+                .keno("0 0-56/4 6-22 * * *")
+                .build();
+    }
+
+    private GameTypeSettingsProperties lottoSettingsProperties() {
+        GameTypeSettingsProperties lottoSettingsProperties = new LottoSettingsProperties();
+        lottoSettingsProperties.setMainNumbersAmount(6);
+        lottoSettingsProperties.setMainNumbersMinValue(1);
+        lottoSettingsProperties.setMainNumbersMaxValue(49);
+        return lottoSettingsProperties;
+    }
+
+    private GameTypeSettingsProperties q600SettingsProperties() {
+        GameTypeSettingsProperties q600SettingsProperties = new LottoSettingsProperties();
+        q600SettingsProperties.setMainNumbersAmount(6);
+        q600SettingsProperties.setMainNumbersMinValue(1);
+        q600SettingsProperties.setMainNumbersMaxValue(32);
+        return q600SettingsProperties;
+    }
+
+    private GameTypeSettingsProperties ejpSettingsProperties() {
+        GameTypeSettingsProperties ejpSettingsProperties = new EjpSettingsProperties();
+        ejpSettingsProperties.setMainNumbersAmount(5);
+        ejpSettingsProperties.setMainNumbersMinValue(1);
+        ejpSettingsProperties.setMainNumbersMaxValue(50);
+        ejpSettingsProperties.setExtraNumbersAmount(2);
+        ejpSettingsProperties.setExtraNumbersMinValue(1);
+        ejpSettingsProperties.setExtraNumbersMaxValue(12);
+        return ejpSettingsProperties;
+    }
+
+    private GameTypeSettingsProperties kenoSettingsProperties() {
+        GameTypeSettingsProperties kenoSettingsProperties = new KenoSettingsProperties();
+        kenoSettingsProperties.setMainNumbersAmount(20);
+        kenoSettingsProperties.setMainTicketNumbersMinAmount(1);
+        kenoSettingsProperties.setMainTicketNumbersMaxAmount(10);
+        kenoSettingsProperties.setMainNumbersMinValue(1);
+        kenoSettingsProperties.setMainNumbersMaxValue(70);
+        return kenoSettingsProperties;
     }
 }
--- src/main/java/pl/wj/lotto/infrastructure/gametype/properties/settings/LottoSettingsProperties.java
+++ src/main/java/pl/wj/lotto/infrastructure/gametype/properties/settings/LottoSettingsProperties.java
@@ -3,4 +3,5 @@ package pl.wj.lotto.infrastructure.gametype.properties.settings;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "lotto.game-type.config.settings.lotto")
-public class LottoSettingsProperties extends GameTypeSettingsProperties {}
+public class LottoSettingsProperties extends GameTypeSettingsProperties {
+}
