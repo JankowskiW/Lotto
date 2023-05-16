c17cada865aafe492791f9d82f948e824f66e6d1
jankowskiw <waldemar.jankowski95@gmail.com>
2023-05-16 21:04:49 +0200

    Configure redis and and caching to draw result checker.

--- src/main/java/pl/wj/lotto/domain/draw/adapter/DrawServiceAdapter.java
+++ src/main/java/pl/wj/lotto/domain/draw/adapter/DrawServiceAdapter.java
@@ -1,6 +1,7 @@
 package pl.wj.lotto.domain.draw.adapter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import pl.wj.lotto.domain.common.gametype.GameType;
 import pl.wj.lotto.domain.draw.model.Draw;
 import pl.wj.lotto.domain.draw.model.dto.DrawResponseDto;
@@ -31,6 +32,7 @@ public class DrawServiceAdapter implements DrawServicePort {
     }
 
     @Override
+    @Cacheable("drawsResults")
     public DrawResultDto getDrawResult(String drawId) {
         return drawService.getDrawResult(drawId);
     }
