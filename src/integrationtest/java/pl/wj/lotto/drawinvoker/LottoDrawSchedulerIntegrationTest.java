f51d59d9dc1f152310c46b16ace9bb380758c619
jankowskiw <waldemar.jankowski95@gmail.com>
2023-05-27 13:55:32 +0200

    Fix scheduler integration tests (fix problem with service url)

--- src/integrationtest/java/pl/wj/lotto/drawinvoker/LottoDrawSchedulerIntegrationTest.java
+++ src/integrationtest/java/pl/wj/lotto/drawinvoker/LottoDrawSchedulerIntegrationTest.java
@@ -3,7 +3,6 @@ package pl.wj.lotto.drawinvoker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import pl.wj.lotto.BaseIntegrationTest;
 import pl.wj.lotto.domain.common.gametype.GameType;
 import pl.wj.lotto.domain.draw.port.in.DrawServicePort;
 import pl.wj.lotto.infrastructure.application.LottoApplication;
@@ -14,8 +13,8 @@ import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@SpringBootTest(classes = LottoApplication.class, properties = "lotto.scheduler.config.enabled=true")
-public class LottoDrawSchedulerIntegrationTest extends BaseIntegrationTest {
+@SpringBootTest(classes = LottoApplication.class, properties = "lotto.game-type.config.interval.lotto=*/1 * * * * *")
+public class LottoDrawSchedulerIntegrationTest {
     @SpyBean
     private DrawServicePort drawServicePort;
 
--- src/integrationtest/java/pl/wj/lotto/drawinvoker/Q600DrawSchedulerIntegrationTest.java
+++ src/integrationtest/java/pl/wj/lotto/drawinvoker/Q600DrawSchedulerIntegrationTest.java
@@ -3,7 +3,6 @@ package pl.wj.lotto.drawinvoker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import pl.wj.lotto.BaseIntegrationTest;
 import pl.wj.lotto.domain.common.gametype.GameType;
 import pl.wj.lotto.domain.draw.port.in.DrawServicePort;
 import pl.wj.lotto.infrastructure.application.LottoApplication;
@@ -15,8 +14,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
-@SpringBootTest(classes = LottoApplication.class, properties = "lotto.scheduler.config.enabled=true")
-public class Q600DrawSchedulerIntegrationTest extends BaseIntegrationTest {
+@SpringBootTest(classes = LottoApplication.class, properties = "lotto.game-type.config.interval.q600=*/1 * * * * *")
+public class Q600DrawSchedulerIntegrationTest {
     @SpyBean
     private DrawServicePort drawServicePort;
 
